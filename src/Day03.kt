bf0c10319b16af2db3b6dac89158d5b7a769ab60
Ga Jun Young <ga.young@personio.de>
2022-12-03 16:48:29 +0000

    [Day03] Part 1 & Part 2 Answers

--- src/Day03.kt
+++ src/Day03.kt
@@ -8,23 +8,41 @@ fun main() {
         }
     }
 
-    val rucksacks = readInput("Day03_test")
-    var priority = 0
+    fun part1(rucksacks: List<String>): Int {
+        var priority = 0
+        rucksacks.forEach { rucksack ->
+            val half = rucksack.length / 2
+            val compartments = listOf(
+                rucksack.substring(0, half).toHashSet(),
+                rucksack.substring(half).toHashSet()
+            )
 
-    for (rucksack in rucksacks) {
-        val half = rucksack.length / 2
-        val compartments = listOf(
-            rucksack.substring(0, half).toHashSet(),
-            rucksack.substring(half).toHashSet()
-        )
-
-        for (item in compartments[0]) {
-            if (compartments[1].contains(item)) {
-                priority += convertToPriority(item)
-                break
+            for (item in compartments[0]) {
+                if (compartments[1].contains(item)) {
+                    priority += convertToPriority(item)
+                    break
+                }
             }
         }
+        return priority
     }
 
-    println(priority)
+    fun part2(rucksacks: List<String>): Int {
+        var priorty = 0
+
+        for (i in rucksacks.indices step 3) {
+            val rucksack1 = rucksacks[i].toHashSet()
+            val rucksack2 = rucksacks[i + 1].toHashSet()
+            val rucksack3 = rucksacks[i + 2].toHashSet()
+
+            val item = rucksack1.intersect(rucksack2).intersect(rucksack3)
+            priorty += convertToPriority(item.first())
+        }
+        return priorty
+    }
+
+    val rucksacks = readInput("Day03_test")
+
+    println(part1(rucksacks))
+    println(part2(rucksacks))
 }
