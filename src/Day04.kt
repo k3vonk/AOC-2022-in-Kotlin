c616b92c5c46914d0e77edba0d099b339639a227
Ga Jun Young <ga.young@personio.de>
2022-12-04 17:35:05 +0000

    [Day04] Part 2 answer

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,28 +1,38 @@
 fun main() {
 
-    fun List<String>.part1(): Int {
-        var count = 0
-        for (assignment in this) {
-            val one = assignment
-                .substringBefore(",")
-                .split("-")
-                .map { it.toInt() }
-            val two = assignment
-                .substringAfter(",")
-                .split("-")
-                .map { it.toInt() }
-
-            val rangeOne = one.first() ..one.last()
-            val rangeTwo = two.first() .. two.last()
-            if (two.first() in rangeOne && two.last() in rangeOne) {
-                count++
-            } else if (one.first() in rangeTwo && one.last() in rangeTwo) {
-                count++
-            }
-        }
-        return count
-    }
     val assignments = readInput("Day04_test")
 
-    println(assignments.part1())
+    var count = 0
+    var part2 = 0
+    for (assignment in assignments) {
+        val one = assignment
+            .substringBefore(",")
+            .split("-")
+            .map { it.toInt() }
+        val two = assignment
+            .substringAfter(",")
+            .split("-")
+            .map { it.toInt() }
+
+        val rangeOne = one.first() ..one.last()
+        val rangeTwo = two.first() .. two.last()
+        if (two.first() in rangeOne && two.last() in rangeOne) {
+            count++
+        } else if (one.first() in rangeTwo && one.last() in rangeTwo) {
+            count++
+        }
+
+        if (two.first() in rangeOne) {
+            part2++
+        } else if (two.last() in rangeOne) {
+            part2++
+        } else if (one.first() in rangeTwo) {
+            part2++
+        } else if (one.last() in rangeTwo) {
+            part2++
+        }
+    }
+
+    println(count)
+    println(part2)
 }
