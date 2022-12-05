a5c8b38e268007e4f107e8d7335613f3f56f0593
Ga Jun Young <ga.young@personio.de>
2022-12-05 19:59:08 +0000

    [Day05] Part 1 Answer

--- src/Day05.kt
+++ src/Day05.kt
@@ -8,17 +8,35 @@ fun main() {
             val arr = crate.toCharArray()
             for (i in arr.indices) {
                 if (arr[i].isLetter()) {
-                    stacks[(i-1)/4].push(arr[i])
+                    stacks[(i-1)/4].add(arr[i])
                 }
             }
         }
 
         return stacks
     }
+
+    fun part1(moves: List<String>, stacks: List<LinkedList<Char>>): String {
+        for (move in moves) {
+            val pattern = "[0-9]+".toRegex()
+            val digits = pattern.findAll(move).map { it.value.toInt() }.toList()
+
+            val numberOfCrates = digits.first()
+            val transition = digits.takeLast(2)
+
+            for(i in 0 until numberOfCrates) {
+                val item = stacks[transition[0] - 1].pop()
+                stacks[transition[1] - 1].push(item)
+            }
+        }
+
+        return stacks.map { it.peek() }.joinToString(separator = "")
+    }
     val data = readInput("Day05_test")
 
     val crates = data.take(8)
     val stacks = createStacks(crates)
+    val moves = data.takeLast(data.size - 10) // I like magic numbers
 
-    println(stacks)
+    println(part1(moves, stacks))
 }
