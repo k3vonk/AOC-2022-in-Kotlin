6598469b25ba03e64affadb67e92961a80c920f7
Ga Jun Young <ga.young@personio.de>
2022-12-11 19:51:58 +0000

    Update day06 solution

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,26 +1,31 @@
 
 fun main() {
 
-    fun hasMatch(input: CharSequence): Boolean {
-        return input.toSet().size == 4
+    fun hasMatch(input: CharSequence, indicator: Int): Boolean {
+        return input.toSet().size == indicator
     }
-    fun part1(input: String): Int {
+
+    fun findStartOfMessageMarker(input: String, marker: Int): Int {
         var substring: CharSequence?
-        var i = 4
+        var i = marker
 
         while(i < input.length) {
-            substring = input.subSequence(i - 4, i)
-            if(hasMatch(substring)) { break }
+            substring = input.subSequence(i - marker, i)
+            if(hasMatch(substring, marker)) { break }
             i++
         }
         return i
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part1(input: String): Int {
+        return findStartOfMessageMarker(input, 4)
+    }
+
+    fun part2(input: String): Int {
+        return findStartOfMessageMarker(input, 14)
     }
 
     val input = readInput("Day06_test")[0]
     println(part1(input))
-
+    println(part2(input))
 }
