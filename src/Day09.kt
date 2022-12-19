68a42c5b8d67442524b40c0ce2e132898683da61
Ga Jun Young <ga.young@personio.de>
2022-12-19 14:08:00 +0000

    [Day09] answer 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -41,41 +41,74 @@ fun main() {
     }
 
     fun part1(input: List<String>): Int {
-        return 0
+        val head = Point(0.0, 0.0)
+        val tail = Point(0.0, 0.0)
+        val visited = mutableListOf(Point(0.0, 0.0))
+
+        for (i in input) {
+            val (direction, count) = i.split(" ")
+
+            for (walk in 0 until count.toInt()) {
+                val directionalPoint = directions[direction]!!
+                head.plus(directionalPoint)
+
+                if (isTwoGridApart(head, tail)) {
+                    if (head.x == tail.x || head.y == tail.y) {
+                        tail.plus(directionalPoint)
+                    } else {
+                        moveTailBasedOnDirection(head, tail)
+                    }
+
+                    if(!visited.contains(Point(tail.x, tail.y))) {
+                        visited.add(Point(tail.x, tail.y))
+                    }
+                }
+
+//                println("head: $head, tail: $tail")
+            }
+        }
+
+        return visited.size
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val ropeknots = MutableList(10) { Point(0.0, 0.0) }
+        val visited = mutableListOf(Point(0.0, 0.0))
+
+        for (i in input) {
+            val (direction, count) = i.split(" ")
+
+            for (walk in 0 until count.toInt()) {
+                val directionalPoint = directions[direction]!!
+
+                ropeknots.forEachIndexed { index, currPoint ->
+
+                    if(index == 0) {
+                        currPoint.plus(directionalPoint)
+                    } else {
+                        val prevKnot = ropeknots[index - 1]
+                        if (isTwoGridApart(prevKnot, currPoint)) {
+                            if (prevKnot.x == currPoint.x || prevKnot.y == currPoint.y) {
+                                val newDirection = Point((prevKnot.x - currPoint.x)/2, (prevKnot.y - currPoint.y)/2)
+                                currPoint.plus(newDirection)
+                            } else {
+                                moveTailBasedOnDirection(prevKnot, currPoint)
+                            }
+
+                            if(!visited.contains(Point(currPoint.x, currPoint.y)) && index == 9) {
+                                visited.add(Point(currPoint.x, currPoint.y))
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return visited.size
     }
 
     val inputs = readInput("Day09_test")
-    var head = Point(0.0, 0.0)
-    var tail = Point(0.0, 0.0)
-    val visited = mutableListOf(Point(0.0, 0.0))
 
-    for (input in inputs) {
-        val (direction, count) = input.split(" ")
-
-        for (walk in 0 until count.toInt()) {
-            val directionalPoint = directions[direction]!!
-            head.plus(directionalPoint)
-
-            if (isTwoGridApart(head, tail)) {
-                if (head.x == tail.x || head.y == tail.y) {
-                    tail.plus(directionalPoint)
-                } else {
-                    moveTailBasedOnDirection(head, tail)
-                }
-
-                if(!visited.contains(Point(tail.x, tail.y))) {
-                    visited.add(Point(tail.x, tail.y))
-                }
-            }
-
-            println("head: $head, tail: $tail")
-        }
-        println(visited.size)
-    }
 
     println(part1(inputs))
     println(part2(inputs))
